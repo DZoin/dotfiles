24b298b72355e61d9a073752fa54683027aaa253
Dimitar Zoin <dimitar.zoin@gmail.com>
2017-10-15 11:25:52 +0300

    Added multiple aliases and functions

--- config.fish
+++ config.fish
@@ -1,8 +1,5 @@
-set PHP_VERSION (ls /usr/local/Cellar/PHP71/7.1.2_13/bin/ | sort -n | tail -1)
-set -x PATH /usr/local/Cellar/PHP71/7.1.2_13/bin/ $PATH
 set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
 set -gx PATH $PATH /usr/local/bin/joe/build
-set -x PATH $PATH /usr/local/bin /usr/local/sbin
 set -gx PATH $PATH $HOME/.composer/vendor/bin
 rvm > /dev/null
 rvm default
@@ -12,4 +9,31 @@ set -gx PATH $PATH /System/Library/Frameworks/JavaScriptCore.framework/Versions/
 alias emacs="/usr/local/Cellar/emacs/25.2/bin/emacs"
 source ~/.config/fish/nvm-wrapper/nvm.fish
 set -gx NVM_DIR ~/.nvm
+set -gx PATH $PATH ~/bin
+set -g fish_user_paths "/usr/local/opt/libxml2/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
+set -gx fish_user_paths "/usr/local/opt/python/libexec/bin" $fish_user_paths
+set -gx fish_user_paths export "homebrew/php/php71" $fish_user_paths
+alias cd..='cd ../'                         # Go back 1 directory level (for fast typers)
+alias ..='cd ../'                           # Go back 1 directory level
+alias ...='cd ../../'                       # Go back 2 directory levels
+alias .3='cd ../../../'                     # Go back 3 directory levels
+alias .4='cd ../../../../'                  # Go back 4 directory levels
+alias .5='cd ../../../../../'               # Go back 5 directory levels
+alias .6='cd ../../../../../../'  			# Go back 6 directory levels
+alias ll='ls -FGlAhp'
+alias f='open -a Finder ./'                 # f: Opens current directory in MacOS Finder
+
+function mcd
+  mkdir -p "$argv[1]"; and cd "$argv[1]";   # mcd: Makes new Dir and jumps inside
+end        
+
+function ll_on_cd --on-variable PWD
+  ll
+end
+
+function trash
+  command mv "$argv[1]" ~/.Trash       
+end
 
